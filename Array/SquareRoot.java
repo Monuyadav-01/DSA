63a2d378d648934088f2bb2116cd9390800ad13d
monu <monuyadav10012000@gmail.com>
2024-01-01 23:33:25 +0530

    leetcode

---
+++ Array/SquareRoot.java
@@ -0,0 +1,30 @@
+public class SquareRoot {
+
+    // public static int mySqrt(int x) {
+
+    // int ans = 0;
+
+    // for (int i = 1; i <= x; i++) {
+    // if (i * i == x) {
+    // ans = i;
+    // break;
+    // }
+
+    // else if (i * i > x) {
+    // ans = i - 1;
+    // break;
+    // }
+    // }
+    // return ans;
+
+    // }
+
+    public static int mySqrt(int x) {
+
+    }
+
+    public static void main(String[] args) {
+        int x = 2147483647;
+        System.out.println(mySqrt(x));
+    }
+}
