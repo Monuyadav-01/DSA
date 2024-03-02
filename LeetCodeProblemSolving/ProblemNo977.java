04e715f8641b9c98d77c3877dcefd356c0bd4351
monu <monuyadav10012000@gmail.com>
2024-03-02 10:20:49 +0530

    problem solved

--- LeetCodeProblemSolving/ProblemNo977.java
+++ LeetCodeProblemSolving/ProblemNo977.java
@@ -1,3 +1,38 @@
-public class ProblemNo977 {
+import java.util.Arrays;
 
+public class ProblemNo977 {
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int result[] = new int[n];
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (rightSquare > leftSquare) {
+                result[index] = rightSquare;
+                right--;
+            } else {
+                result[index] = leftSquare;
+                left++;
+            }
+            index--;
+        }
+
+        return nums;
+
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { -4, -1, 0, 3, 10 };
+        int res[] = sortedSquares(nums);
+
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
+    }
 }
